5e26ba7299db93afc41a5aed675acef8ef0e5c05
ben kipkulei <benkaimugul@gmail.com>
2023-03-09 17:44:34 +0300

    todays code

---
+++ nester/import os.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import time
+
+def rename_file(path):
+    folder_name = os.path.basename(os.path.dirname(path))
+    create_time = os.path.getctime(path)
+    date_time = time.strftime("%Y%m%d-%H%M%S", time.gmtime(create_time))
+    file_name = os.path.basename(path)
+    new_name = f"{folder_name}_{date_time}_{file_name}"
+    new_path = os.path.join(os.path.dirname(path), new_name)
+    shutil.move(path, new_path)
+
+# Example usage
+folder_path = "G:\My Drive\KAMEMEME VM"
+for file_name in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, file_name)
+    if os.path.isfile(file_path):
+        rename_file(file_path)
---
+++ nester/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+name= 'nester',
+version='1.0.0',
+py_modules=['nester'],
+author='hfpython',
+author_email='benkaimugul@gmail.com',
+url= 'babu.com',
+description='A simpe printer of nested lists',
+)
