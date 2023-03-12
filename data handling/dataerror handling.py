3f3a175801409c068bd72efd464e053ff443369b
babu97 <benkaimugul@gmail.com>
2023-03-12 13:00:35 +0300

    modified error handling

--- data handling/dataerror handling.py
+++ data handling/dataerror handling.py
@@ -1,15 +1,18 @@
-## this uses try statements
+## this uses try statements to sort out errors
 import os
 os.chdir('/home/babu97/HeadFirstpython/chapter3')
+try:
+    data=open('sketch.txt')
+    for each_line in data:
+        try:
+            (role,line_spoken)= each_line.split(':',1)
+            print(role, end='')
+            print(' said: ' ,end='')
+            print(line_spoken, end='')
+        except:
+            pass
+except:
+    print ('The data file is missing!')
 
-data=open('sketch.txt')
-for each_line in data:
-    try:
-        (role,line_spoken)= each_line.split(':',1)
-        print(role, end='')
-        print(' said: ' ,end='')
-        print(line_spoken, end='')
-    except:
-        pass
 data.close()
 
--- data handling/errorrecovery.py
+++ data handling/errorrecovery.py
@@ -2,12 +2,15 @@
 
 import os
 os.chdir('/home/babu97/HeadFirstpython/chapter3')
+if os.path.exists('sketch.txt'):
+    data=open('sketch.txt')
+    for each_line in data:
+        if not each_line.find(':') == -1:
+            (role,line_spoken) = each_line.split(':',1)
+            print(role, end='')
+            print(' said: ', end='')
+            print(line_spoken, end='')
+else:
+    print(' error!! 404: file not found !! please check provided')
 
-data=open('sketch.txt')
-for each_line in data:
-    if not each_line.find(':') == -1:
-        (role,line_spoken) = each_line.split(':',1)
-        print(role, end='')
-        print(' said: ', end='')
-        print(line_spoken, end='')
 data.close()
